f99b91606e6f1f79fe55847df017d024704f0eb7
Peppi-Lotta Saari <peppilotta.saari@gmail.com>
2024-01-17 16:24:36 +0200

    some general styles and 2024 fronpage stuff

--- src/lib/components/header.svelte
+++ src/lib/components/header.svelte
@@ -1,16 +1,96 @@
 <script>
-    import '../../style/app.scss'
+	import { onMount } from 'svelte';
+	import logo from '../images/logo_white.png';
+
+	let menuItems = []; // Define menuItems
+
+	const getTopLevelPages = async () => {
+		const query = `
+        {
+            pages(where: {parent: null}) {
+                nodes {
+                    id
+                    title
+                    uri
+                }
+            }
+        }
+        `;
+
+		const response = await fetch(import.meta.env.VITE_PUBLIC_WORDPRESS_API_URL, {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify({
+				query
+			})
+		});
+
+		const data = await response.json();
+
+		if (!data.data || !data.data.pages) {
+			console.error('Error fetching pages:', data.errors);
+			return [];
+		}
+
+		return data.data.pages.nodes;
+	};
+
+	onMount(async () => {
+		menuItems = await getTopLevelPages();
+	});
 </script>
 
+<!-- Then, in your markup, you can loop through menuItems to create your navigation menu -->
 <div class="header">
-    <div>
-        <h1>yläpalkki</h1>
-    </div>
+	<a href="/" class="logo">
+		<img src={logo} alt="Logo" />
+    </a>
 
-    <nav>
-        <span><a href="/">etusivu</a></span>
-        <span><a href="/historia">historia</a></span>
-    </nav>
+	<nav>
+		{#each menuItems as item (item.id)}
+			<a href={item.uri}>{item.title}</a>
+		{/each}
+	</nav>
 </div>
 
+<style lang="scss">
+    @use '../../style/variables' as v;
+	.header {
+		background-color: rgba(v.$black, 0.5);
+        backdrop-filter: blur(10px);
+		display: flex;
+		flex-direction: row;
+		justify-content: space-between;
+		align-items: center;
+		padding-left: 20px;
+		padding-right: 20px;
 
+		nav {
+			display: flex;
+			flex-direction: row;
+
+			span {
+				padding: 10px;
+			}
+		}
+	}
+
+	.logo {
+		height: 70px;
+		img {
+			height: 100%;
+		}
+	}
+
+	nav {
+		display: flex;
+		gap: 1rem;
+	}
+
+	nav a {
+		color: #fff;
+		text-decoration: none;
+	}
+</style>
---
+++ src/routes/kevatpaivanseisaus/+page.svelte
@@ -0,0 +1,75 @@
+<script>
+</script>
+
+<div class="production-logo">
+	<h1 class="neon">
+		<div>KE<span class="flicker-slow">V</span>ÄT</div>
+		<div>P<span class="flicker-slow">Ä</span>IVÄN</div>
+		<div>SEI<span class="flicker-slow">S</span>A<span class="flicker-fast u-turn">U</span>S</div>
+	</h1>
+</div>
+
+<style>
+	.production-logo {
+		text-align: center;
+	}
+
+	@font-face {
+		font-family: 'Beon';
+		src: url('../../lib/fonts/BeonMedium-6d51.otf') format('truetype');
+	}
+
+	.u-turn {
+		display: inline-block;
+		transform: translateY(50%) rotate(45deg);
+		transform-origin: center center;
+	}
+
+	.neon {
+		font-family: 'Beon', sans-serif;
+		font-size: 130px;
+		color: #ffffff;
+		font-weight: 400;
+		letter-spacing: 8px;
+		text-shadow:
+			1px 0px 4px #fdcefd,
+			2px 0px 4px rgb(253, 198, 253),
+			3px 0px 4px rgb(255, 154, 255),
+			2px 0px 3px #c92cbe,
+			2px 3px 15px #c415b8,
+			2px 0px 15px,
+			5px 0px 125px,
+			20px 0vw 200px #d42cca,
+			40px 0vw 200px #b826ae;
+	}
+
+	.flicker-slow {
+		animation: flicker 3s linear infinite;
+	}
+
+	.flicker-fast {
+		animation: flicker 1s linear infinite;
+	}
+
+	@keyframes flicker {
+		0%,
+		19.999%,
+		22%,
+		62.999%,
+		64%,
+		64.999%,
+		70%,
+		100% {
+			//text-shadow: none;
+			opacity: 0.99;
+		}
+		20%,
+		21.999%,
+		63%,
+		63.999%,
+		65%,
+		69.999% {
+			opacity: 0.4;
+		}
+	}
+</style>
