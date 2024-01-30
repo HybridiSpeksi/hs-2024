4d3e10f044b306300e0855da8447d8a13e15ab37
allurei <suhonen.laakso@gmail.com>
2024-01-30 18:05:56 +0200

    yhteydenotto and people ugly

---
+++ src/routes/yhteydenotto/+page.server.js
@@ -0,0 +1,66 @@
+export const load = async({ }) => {
+    const query = `
+    {
+        people {
+          nodes {
+            personFields {
+              jobs
+              name
+              type
+              image {
+                node {
+                  mediaItemUrl
+                }
+              }
+            }
+          }
+        }
+      }
+    `;
+
+    const response = await fetch(import.meta.env.VITE_PUBLIC_WORDPRESS_API_URL, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            query
+        })
+    });
+
+    const data = await response.json();
+
+    if (!data.data?.people) {
+        console.error('Error fetching pages:', data.errors);
+        return {
+           people: {}
+        }
+    }
+    let people = groupByName(data.data.people.nodes)
+
+    return {
+        people
+    };
+}
+
+
+const groupByName = (array) => {
+  // Predefined categories
+  const predefinedCategories = ['hallitus', 'toimihenkilo', 'tuotantotiimi'];
+
+  // Initialize the result object with predefined categories
+  const result = {};
+  predefinedCategories.forEach(category => {
+    result[category] = [];
+  });
+
+  // Group the rest of the objects by name
+  array.forEach(obj => {
+    if (!result[obj.personFields.type[0]]) {
+      result[obj.personFields.type[0]] = [];
+    }
+    result[obj.personFields.type[0]].push(obj);
+  });
+
+  return result;
+};
---
+++ src/routes/yhteydenotto/+page.svelte
@@ -0,0 +1,43 @@
+<script>
+    export let data;
+    const people = data.people
+    console.log(people)
+
+    function capitalizeFirstLetter(str) {
+    // Check if the string is empty or null
+    if (!str) return str;
+
+    // Capitalize the first letter and concatenate it with the rest of the string
+    return str.charAt(0).toUpperCase() + str.slice(1);
+    }
+</script>
+
+<div>
+    {#if typeof people === 'object'  && Object.keys(people)}
+    {#each Object.keys(people) as group}
+    <div class="{group}">
+        <h2>{capitalizeFirstLetter(group)}</h2>
+        {#if people[group]}
+            {#each people[group] as person}
+            <div class = "aaaa">
+                {person.personFields.name}
+                {person.personFields.jobs}
+                {#if person.personFields.image?.node}
+                    {person.personFields.image.node.mediaItemUrl}
+                {/if}
+            </div>
+            {/each}
+        {/if}
+    </div>
+        
+    {/each}
+    {/if}
+</div>
+
+
+<style lang="scss">
+    .aaaa{
+        margin: 20px;
+        background-color: red;
+    }
+</style>
