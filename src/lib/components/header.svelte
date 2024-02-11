0c0ecc6d0a175ca817e13c97c7d97b6266c3151d
allurei <suhonen.laakso@gmail.com>
2024-02-11 15:16:50 +0200

    header nav

--- src/lib/components/header.svelte
+++ src/lib/components/header.svelte
@@ -1,23 +1,63 @@
 <script>
-	import logo from '../images/logo_white.png';
-	
+	import logo from '$lib/images/logo_white.png';
+	import { afterNavigate } from '$app/navigation';
+
 	export let data;
 	
 	//These are fetched in +layout.server.js
 	let menuItems = data.nodes;
+
+	let x = 0;
+
+	let active = false;
+
+	afterNavigate(() => (active = false));
+
+	function menuToggle() {
+		active = !active
+		if (active) {
+			document.body.classList.add("overflow-y-hidden")
+		} else {
+			document.body.classList.remove("overflow-y-hidden")
+		}
+	}
+
 </script>
+
+<svelte:window bind:innerWidth={x}></svelte:window>
 
 <!-- Then, in your markup, you can loop through menuItems to create your navigation menu -->
 <div class="header">
 	<a href="/" class="logo">
 		<img src={logo} alt="Logo" />
 	</a>
+	{#if x < 700}
+	<div class="mobile-nav" class:active>
+		<div class="menu-toggle" class:active on:click={menuToggle} on:keydown={(e) => {e.code ==="Enter" ? active = !active : '' }} role="button" tabindex="0">
+			<div>
+				<div class="con">
+					<div class="bar top"></div>
+					<div class="bar middle"></div>
+					<div class="bar bottom"></div>
+				</div>
+			</div>
+		</div>
 
+		<nav class:active>
+			<ul>
+				{#each menuItems as item (item.id)}
+					<li><a href={item.uri}>{item.title}</a></li>
+				{/each}
+			</ul>
+		</nav>
+	</div>
+	{:else}
 	<nav>
 		{#each menuItems as item (item.id)}
 			<a href={item.uri}>{item.title}</a>
 		{/each}
 	</nav>
+	{/if}
 </div>
 
 <style lang="scss">
@@ -35,10 +75,6 @@
 		nav {
 			display: flex;
 			flex-direction: row;
-
-			span {
-				padding: 10px;
-			}
 		}
 	}
 
@@ -57,5 +93,73 @@
 	nav a {
 		color: #fff;
 		text-decoration: none;
+	}
+
+	.mobile-nav{
+		nav {
+			display: none;
+			&.active {
+				display: flex;
+				flex-direction: column;
+				width: 100%;
+				position: absolute;
+				top: 70px;
+				left: 0;
+				height: calc(100vh - 70px);
+				z-index: 10;
+				background-color: black;
+				ul {
+					padding-top: 50px;
+					display: flex;
+					flex-direction: column;
+					margin: 0 auto;
+					list-style: none;
+					li {
+						font-size: 24px;
+						margin: 10px;
+					}
+				}
+			}
+		}
+		.menu-toggle {
+			display: flex;
+			position: fixed;
+			top: 0;
+			right: 0;
+			height: 60px;
+			align-items: center;
+			>div {
+				width: 60px;
+			}
+			.con {
+				width: auto;
+				margin: 0 auto;
+				.bar {
+					display: block;
+					height: 3px;
+					width: 28px;
+					margin: 5px auto;
+					border-radius: 2px;
+					background-color: #fff;
+					transition: all 0.15s ease;
+				}
+			}
+			&.active {
+				.con {
+					.bar {
+						&.top {
+							color: pink;
+							transform: translateY(4px) rotateZ(45deg);
+						}
+						&.middle {
+							display: none;
+						}
+						&.bottom {
+							transform: translateY(-4px) rotateZ(-45deg);
+						}
+					}
+				}
+			}
+		}
 	}
 </style>
