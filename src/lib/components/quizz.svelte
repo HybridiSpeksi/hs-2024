ac0e5dccdf05f1d93c4a38038de04ad11fb32fd6
Peppi-Lotta Saari <peppilotta.saari@gmail.com>
2024-03-03 15:19:39 +0200

    initialize quizz

---
+++ src/lib/components/quizz.svelte
@@ -0,0 +1,14 @@
+<script>
+	let data = null;
+    export let id = 0;
+
+fetch(`https://wp.hybridispeksi.fi//wp-json/ifeelquizzy/v1/data/${id}`)
+		.then((response) => response.json())
+		.then((json) => (data = json))
+		.catch((error) => console.error('Error:', error));
+    
+    console.log(data);
+</script>
+
+<style>
+</style>
---
+++ src/routes/quizz_test/test.svelte
@@ -0,0 +1,7 @@
+<script>
+    import Quizz from '$lib/components/quizz.svelte';
+
+    let id = 2;
+</script>
+
+<Quizz {id} />
